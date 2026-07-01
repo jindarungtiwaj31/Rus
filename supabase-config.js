1db6c4d28b225505b9058eb4f914fde97d67fac5
jindarungtiwaj31 <jindarungtiwa31@gmail.com>
2026-07-01 08:27:22 +0700

    Configure Supabase project

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
 window.RECEIPT_APP_SUPABASE = {
-  url: "",
-  anonKey: ""
+  url: "https://hmpjgnshmzhbuaxnkmcz.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhtcGpnbnNobXpoYnVheG5rbWN6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI4Mzg4MjksImV4cCI6MjA5ODQxNDgyOX0.KQZNBiIaIg9_2p4OkOO2OBWIvWnB7NRODn2kcfaKpxQ"
 };
